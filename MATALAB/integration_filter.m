2366bba96e7870b62b18cc3aec98c2182f7c080e
Willian <willianbarelac@gmail.com>
2019-07-11 08:02:03 -0300

    ateração de mudanças de diretórios

--- MATALAB/integration_filter.m
+++
@@ -1,8 +0,0 @@
-function [Filterd_Output] = integration_filter(data,fs)
-
-d=fdesign.lowpass('Fp,Fst,Ap,Ast', 100, 110,1,20, fs);
- designmethods(d);
- Hd = design(d,'equiripple');
- %fvtool(Hd);
- Filterd_Output = filter(Hd,data);
- s_fft(Filterd_Output,fs);
